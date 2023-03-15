9d3b00e91c1f0ea39ec01d5755d8fa3ebb70d7df
Kaden Fedele <111700857+KadenFedele@users.noreply.github.com>
2023-03-15 16:02:06 +0000

    First commit

---
+++ js/expanding.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorALL('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActive()
+        panel.classList.add('active')
+    })
+})
+
+
+
+function removeActve() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
