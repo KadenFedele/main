fff0c67aa5476fd37ef4f74db1a891476a107a39
Kaden Fedele <111700857+KadenFedele@users.noreply.github.com>
2023-03-24 17:03:28 +0000

    Fri, Mar 24, 2023, 5:03 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'Timme Owns UCLA'
+const text = 'This is text that goes fast'
 let idx = 1
 let speed = 300 / speedEl.value
 
