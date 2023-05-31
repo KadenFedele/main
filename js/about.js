c3f3ec999a6b82e36ff958c7b85f95c1f0cfee45
Kaden Fedele <111700857+KadenFedele@users.noreply.github.com>
2023-05-31 05:00:22 +0000

    Wed, May 31, 2023, 5:00 AM +00:00

--- js/about.js
+++ js/about.js
@@ -1,10 +1,3 @@
-// Add an event listener to the "About Me" paragraph
-var aboutMe = document.querySelector("main p");
-aboutMe.addEventListener("click", function() {
-  alert("You clicked on the 'About Me' paragraph!");
-});
-
-// Change the background color of the navigation links on hover
 var navLinks = document.querySelectorAll("nav a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("mouseover", function() {
